7c788050f21f8fce97e96863298213a7dcbdecfe
washingtoncandeia <araujowash@gmail.com>
2020-01-18 17:44:01 -0300

    Todas as amostras Zika + GBS + GBS-rec

---
+++ DESeq2_GSEA_DF/ZGBSallSYMBOL.R
@@ -0,0 +1,350 @@
+##---------------------------------------------
+# Análise em Nível de Transcrito Kallisto
+# Utilizando Octuplicatas
+# alpha = 0.05 (FDR, padj < 0.05) + IWH
+# Wald test p-value: condition gbs_rec vs control
+# Data: 18/01/2020
+##---------------------------------------------
+library(tximport)
+library(DESeq2)
+library(readr)
+library(dplyr)
+library(rhdf5)
+library(IHW)
+
+
+## Parte 1 - Preparação de dados das amostras de kallisto.
+# Caminho dos arquivos (fele path)
+dir <- './results'
+list.files(dir)
+
+# Nomes de populações
+ZIKA <- 'ZIKA'
+CHIKV <- 'CHIKV'
+CHIKV_REC <- 'CHIKV_REC'
+GBS <- 'GBS'
+GBS_REC <- 'GBS_REC'
+CONTROL <- 'CONTROL'
+
+
+# Vetor com nomes populações para fazer coluna pop:
+# Obs.: Ao fazer o vetor, melhor deixar organizado combinando amostra + nomearquivo
+# Coluna 1
+pop <- c(rep(GBS, 8),      # GBS        1
+         rep(CONTROL, 8),  # Control   10
+         rep(CONTROL, 8),  # Control   11
+         rep(ZIKA, 8),     # Zika      12
+         rep(GBS, 8),      # GBS       13
+         rep(GBS, 8),      # GBS       14
+         rep(GBS_REC, 8),  # GBSrec    16 
+         rep(CHIKV_REC, 8),# Chikv rec 18
+         rep(CONTROL, 8),  # Control   19
+         rep(GBS, 8),      # GBS        2
+         rep(CONTROL, 8),  # Control   20
+         rep(CHIKV, 8),    # Chikv     22
+         rep(ZIKA, 8),     # Zika      24
+         rep(GBS, 8),      # GBS       25
+         rep(GBS_REC, 8),  # GBSrec    27
+         rep(GBS_REC, 8),  # GBSrec    28
+         rep(GBS_REC, 8),  # GBSrec    30
+         rep(CHIKV, 8),    # Chikv     31
+         rep(CHIKV, 8),    # Chikv     32
+         rep(CONTROL, 8),  # Control   33  
+         rep(CONTROL, 8),  # Control   34
+         rep(CHIKV_REC, 8),# Chikv rec 35
+         rep(ZIKA, 8),     # Zika      36
+         rep(GBS_REC, 8),  # GBSrec    38
+         rep(GBS_REC, 8),  # GBSrec    39
+         rep(GBS, 8),      # GBS        4
+         rep(CHIKV, 8),    # Chikv     40
+         rep(CHIKV_REC, 8),# Chikv rec 41
+         rep(CHIKV_REC, 8),# Chikv rec 42
+         rep(CHIKV_REC, 8),# Chikv rec 46
+         rep(CHIKV_REC, 8),# Chikv rec 47
+         rep(ZIKA, 8),     # Zika      48
+         rep(GBS_REC, 8),  # GBSrec     7
+         rep(GBS_REC, 8),  # GBSrec     8
+         rep(GBS_REC, 8))  # GBSrec     9
+
+head(pop, 12)    # ZIKA, CHIKV, CHIKV_REC, GBS, GBS_REC, CONTROL
+length(pop)      # 280
+
+
+# Coluna 2
+# Nome de centro de pesquisa para fazer coluna center
+center <- rep('IMT-UFRN', 280)
+head(center)
+length(center)   # 280
+
+# Coluna 3
+# Nomes de amostras analisadas para fazer coluna run
+run <- list.files(dir)
+head(run)
+mode(run)
+length(run)      # 280
+
+
+# Coluna 4
+condition <- c(rep('gbs', 8),       # GBS        1
+               rep('control', 8),   # Control   10
+               rep('control', 8),   # Control   11
+               rep('zika', 8),      # Zika      12
+               rep('gbs', 8),       # GBS       13
+               rep('gbs', 8),       # GBS       14
+               rep('gbs_rec', 8),   # GBSrec    16 
+               rep('chikv_rec', 8), # Chikv rec 18
+               rep('control', 8),   # Control   19
+               rep('gbs', 8),       # GBS        2
+               rep('control', 8),   # Control   20
+               rep('chikv', 8),     # Chikv     22
+               rep('zika', 8),      # Zika      24
+               rep('gbs', 8),       # GBS       25
+               rep('gbs_rec', 8),   # GBSrec    27
+               rep('gbs_rec', 8),   # GBSrec    28
+               rep('gbs_rec', 8),   # GBSrec    30
+               rep('chikv', 8),     # Chikv     31
+               rep('chikv', 8),     # Chikv     32
+               rep('control', 8),   # Control   33  
+               rep('control', 8),   # Control   34
+               rep('chikv_rec', 8), # Chikv rec 35
+               rep('zika', 8),      # Zika      36
+               rep('gbs_rec', 8),   # GBSrec    38
+               rep('gbs_rec', 8),   # GBSrec    39
+               rep('gbs', 8),       # GBS        4
+               rep('chikv', 8),     # Chikv     40
+               rep('chikv_rec', 8), # Chikv rec 41
+               rep('chikv_rec', 8), # Chikv rec 42
+               rep('chikv_rec', 8), # Chikv rec 46
+               rep('chikv_rec', 8), # Chikv rec 47
+               rep('zika', 8),      # Zika      48
+               rep('gbs_rec', 8),   # GBSrec     7
+               rep('gbs_rec', 8),   # GBSrec     8
+               rep('gbs_rec', 8))   # GBSrec     9
+
+length(condition)
+
+
+# Coluna 5
+# Pares
+pares <- c(rep('não', 8),  # GBS        1
+           rep('não', 8),  # Control   10
+           rep('não', 8),  # Control   11
+           rep('não', 8),  # Zika      12
+           rep('não', 8),  # GBS       13
+           rep('não', 8),  # GBS       14
+           rep('par', 8),  # GBSrec    16 
+           rep('não', 8),  # Chikv rec 18
+           rep('não', 8),  # Control   19
+           rep('não', 8),  # GBS        2
+           rep('não', 8),  # Control   20
+           rep('não', 8),  # Chikv     22
+           rep('não', 8),  # Zika      24
+           rep('não', 8),  # GBS       25
+           rep('não', 8),  # GBSrec    27
+           rep('não', 8),  # GBSrec    28
+           rep('par', 8),  # GBSrec    30
+           rep('não', 8),  # Chikv     31
+           rep('não', 8),  # Chikv     32
+           rep('não', 8),  # Control   33  
+           rep('não', 8),  # Control   34
+           rep('não', 8),  # Chikv rec 35
+           rep('não', 8),  # Zika      36
+           rep('não', 8),  # GBSrec    38
+           rep('não', 8),  # GBSrec    39
+           rep('não', 8),  # GBS        4
+           rep('não', 8),  # Chikv     40
+           rep('não', 8),  # Chikv rec 41
+           rep('não', 8),  # Chikv rec 42
+           rep('não', 8),  # Chikv rec 46
+           rep('não', 8),  # Chikv rec 47
+           rep('não', 8),  # Zika      48
+           rep('par', 8),  # GBSrec     7
+           rep('não', 8),  # GBSrec     8
+           rep('par', 8))  # GBSrec     9
+
+length(pares)
+
+# Coluna 6
+# Tipo GBS: desmielinizante x não desmielinizante
+# Pares de condições
+diagn <- c(rep('Desmielin', 8),   # GBS        1
+           rep('não', 8),         # Control   10
+           rep('não', 8),         # Control   11
+           rep('não', 8),         # Zika      12
+           rep('Desmielin', 8),   # GBS       13
+           rep('Desmielin', 8),   # GBS       14
+           rep('Desmielin', 8),   # GBSrec    16 
+           rep('não', 8),         # Chikv rec 18
+           rep('não', 8),         # Control   19
+           rep('Desmielin', 8),   # GBS        2
+           rep('não', 8),         # Control   20
+           rep('não', 8),         # Chikv     22
+           rep('não', 8),         # Zika      24
+           rep('Desmielin', 8),   # GBS       25
+           rep('Desmielin', 8),   # GBSrec    27
+           rep('Desmielin', 8),   # GBSrec    28
+           rep('Inconclus', 8),   # GBSrec    80
+           rep('não', 8),         # Chikv     31
+           rep('não', 8),         # Chikv     32
+           rep('não', 8),         # Control   33  
+           rep('não', 8),         # Control   34
+           rep('não', 8),         # Chikv rec 35
+           rep('não', 8),         # Zika      36
+           rep('Desmielin', 8),   # GBSrec    38
+           rep('Axonal', 8),      # GBSrec    39
+           rep('Desmielin', 8),   # GBS        4
+           rep('não', 8),         # Chikv     40
+           rep('não', 8),         # Chikv rec 41
+           rep('não', 8),         # Chikv rec 42
+           rep('não', 8),         # Chikv rec 46
+           rep('não', 8),         # Chikv rec 47
+           rep('não', 8),         # Zika      48
+           rep('Desmielin', 8),   # GBSrec     7
+           rep('Desmielin', 8),   # GBSrec     8
+           rep('Desmielin', 8))   # GBSrec     9
+
+
+length(diagn)
+
+# Coluna 7
+# Sexo
+sex <- c(rep('Fem', 8),  # GBS        1
+         rep('Fem', 8),  # Control   10
+         rep('Mas', 8),  # Control   11
+         rep('Fem', 8),  # Zika      12
+         rep('Mas', 8),  # GBS       13
+         rep('Mas', 8),  # GBS       14
+         rep('Mas', 8),  # GBSrec    16 
+         rep('Fem', 8),  # Chikv rec 18
+         rep('Mas', 8),  # Control   19
+         rep('Fem', 8),  # GBS        2
+         rep('Mas', 8),  # Control   20
+         rep('Fem', 8),  # Chikv     22
+         rep('Fem', 8),  # Zika      24
+         rep('Fem', 8),  # GBS       25
+         rep('Fem', 8),  # GBSrec    27
+         rep('Mas', 8),  # GBSrec    28
+         rep('Fem', 8),  # GBSrec    30
+         rep('Mas', 8),  # Chikv     31
+         rep('Fem', 8),  # Chikv     32
+         rep('Fem', 8),  # Control   33  
+         rep('Mas', 8),  # Control   34
+         rep('Fem', 8),  # Chikv rec 35
+         rep('Fem', 8),  # Zika      36
+         rep('Mas', 8),  # GBSrec    38
+         rep('Fem', 8),  # GBSrec    39
+         rep('Fem', 8),  # GBS        4
+         rep('Mas', 8),  # Chikv     40
+         rep('Fem', 8),  # Chikv rec 41
+         rep('Fem', 8),  # Chikv rec 42
+         rep('Fem', 8),  # Chikv rec 46
+         rep('Fem', 8),  # Chikv rec 47
+         rep('Fem', 8),  # Zika      48
+         rep('Fem', 8),  # GBSrec     7
+         rep('Mas', 8),  # GBSrec     8
+         rep('Fem', 8))  # GBSrec     9
+
+length(sex)
+
+
+# Coluna 8
+# Replicatas
+replicates <- c('rep01', 'rep02', 'rep03 ', 'rep04',
+                'rep05', 'rep06 ', 'rep07', 'rep08')
+
+## Parte II
+# Aqui inicia-se a construção do data frame com todas as informações de amostras.
+## Parte 2 - Unir cada vetor formando colunas de um data frame:
+samples_info <- data.frame(pop = pop,
+                           center = center,
+                           run = run,
+                           condition = condition,
+                           diagnostico = diagn,
+                           pair = pares,
+                           sex = sex,
+                           replicate = rep(replicates, 35))   
+
+#
+# Observações gerais:
+head(samples_info, 10)
+str(samples_info)
+names(samples_info)
+
+## -------------------- Eliminando Linhas do Data Frame por Nomes  -------------------- ##
+## Quando necessário o uso de um data frame menor, com apenas algumas variáveis.
+
+# Usando dplyr, função filter e negando com regex (função grepl)
+
+samples_info <- samples_info %>% 
+  filter(!grepl(CHIKV_REC, pop))
+
+samples_info <- samples_info %>% 
+  filter(!grepl(CHIKV, pop))
+
+samples_info
+
+## ------------------------------------------------------------------------------------ ##
+length(samples_info$condition)
+# Salvar a tabela no formato .txt (tsv)
+write.table(samples_info, './tables/zgbs/zika_gbs_vs_control.txt', sep = '\t')
+
+# Criar um vetor nomeado apontando os arquivos de quantificação.
+# Estes arquivos têm seus nomes anotados em uma tabela (samples.txt).
+samples <- read.table('./tables/zgbs/zika_gbs_vs_control.txt', header = TRUE, row.names = 1)
+head(samples, 9)
+samples$condition 
+
+# Relevel: ajustando a condição referência para análise
+samples$condition <- relevel(samples$condition, ref = 'control')
+
+# Nomeando as linhas com nome de cada arquivo de amostra:
+rownames(samples) <- samples$run
+head(samples, 25)
+
+# Obtendo cada arquivo de replicata das amostras usadas em kallisto:
+files <- file.path(dir, samples$run, 'abundance.h5')
+files
+names(files) <- samples$run
+head(files, 9)
+
+# Criação de IDs Ensembl
+# Arquivo t2g data frame
+t2g <- read_csv('./t2sg.csv')
+head(t2g, 12)
+nrow(t2g)
+
+### Parte III - Tximport Utlizando Arquivos kallisto
+txi.kallisto <- tximport(files, 
+                         type = 'kallisto',
+                         tx2gene = t2g)
+
+
+# Observações gerais
+names(txi.kallisto)
+head(txi.kallisto$abundance)
+
+#### Parte IV - DESeq2
+## Design com formula simples:
+dds.txi <- DESeqDataSetFromTximport(txi = txi.kallisto,
+                                    colData = samples,
+                                    design = ~condition)
+
+## Pre-filtering
+# Remover genes sem contagens significantes para as amostras. Neste caso, no mínimo 10 contagens.
+keep <- rowSums(counts(dds.txi)) >= 10
+dds <- dds.txi[keep, ]
+head(dds$condition, 9)
+
+### Análise de Expressão Diferencial (DE)
+# Objeto dds por DESeq2
+# Modelando as contagens com efeitos de condition vs control
+dds <- DESeq(dds)
+res <- results(dds, filterFun=ihw, alpha = 0.05)
+res.gbs <- results(dds, contrast = c('condition', 'gbs', 'control'), filterFun=ihw, alpha = 0.05)
+res.gbs.rec <- results(dds, contrast = c('condition', 'gbs_rec', 'control'), filterFun=ihw, alpha = 0.05)
+
+summary(res)
+
+write.csv(as.data.frame(res), file = './GSEA/fgsea_ZGBS_zika_vs_control_SYMBOL.csv')
+write.csv(as.data.frame(res.gbs), file = './GSEA/fgsea_ZGBS_gbs_vs_control_SYMBOL.csv')
+write.csv(as.data.frame(res.gbs.rec), file = './GSEA/fgsea_ZGBS_gbs_rec_vs_control_SYMBOL.csv')
